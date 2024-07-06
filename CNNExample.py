7d5e7245902a253ecb5cfde1266d26a42f1a5956
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-07-06 15:01:58 -0400

    Cont.

---
+++ CNNExample.py
@@ -0,0 +1,7 @@
+import os
+import zipfile
+import random
+import tensorflow as tf
+import tensorflow.keras.optimizers as optimizers
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from shutil import copyfile
